8a0beedd64018577cf671444de0ccfec56240ae5
hajieew <hajieew@gmail.com>
2026-04-25 23:49:49 +0400

    Salim

--- part2/models/amenity.py
+++
@@ -1,9 +0,0 @@
-from models.base import BaseModel
-
-class Amenity(BaseModel):
-    def __init__(self, name):
-        super().__init__()
-        self.name = name
-
-    def to_dict(self):
-        return self.__dict__
